5b747b97a09cc5182a10c81400f1bf21412b9bda
natsunoyuki <user@192.168.1.8>
2025-02-17 14:58:30 +0800

    Added custom loss functions.

---
+++ src/unet/loss_functions/focal_loss.py
@@ -0,0 +1,20 @@
+# https://pytorch.org/vision/main/generated/torchvision.ops.sigmoid_focal_loss.html
+# Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
+
+import torchvision.ops.sigmoid_focal_loss
+
+
+class FocalLoss():
+    def __init__(self, alpha=0.25, gamma=2, reduction="mean"):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+    
+    def __call__(self, inputs, targets):
+        return torchvision.ops.sigmoid_focal_loss(
+            inputs, 
+            targets, 
+            alpha=self.alpha, 
+            gamma=self.gamma, 
+            reduction=self.reduction,
+        ) 
